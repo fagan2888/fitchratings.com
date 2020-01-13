50344acc74a163dd78d7d3da9c705b4a1b6822fd
rifansetiawan <37042039+rifansetiawan@users.noreply.github.com>
2020-01-13 14:14:06 +0700

    Create char.py

---
+++ char.py
@@ -0,0 +1,11 @@
+#import pandas
+str = "7 F1+(idn)"
+substring1="idn"
+boleaannya = str.count(substring1)
+#print(str.count(substring1))
+if boleaannya == 1:
+    print(str)
+
+
+
+##----->ini acuan tok
